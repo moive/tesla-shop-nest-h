1696321f0b8f083dcb8c9dc0e1642fc98f9a662a
Moises Velasquez <moises007@gmail.com>
2025-08-20 00:24:14 -0500

    assetsProducts

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -5,7 +5,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 @Injectable()
 export class FilesService {
   getStaticFileUploadImage(imageName: string) {
-    const path = join(__dirname, '../../static/uploads', imageName);
+    const path = join(__dirname, '../../static/products', imageName);
 
     if (!existsSync(path))
       throw new BadRequestException(`No product found with image ${imageName}`);
